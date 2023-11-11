04fbfc033393bf9f79b91971deabdf15ba9f5c5f
koushalm24 <skoushal4301@gmail.com>
2023-11-11 19:04:46 +0530

    changed js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var image = document.querySelector("#sova>img");
-var p1 = document.querySelector("#page1");
+// var image = document.querySelector("#sova>img");
+// var p1 = document.querySelector("#page1");
 
 var sound1 = document.getElementById("rervealingarera");
 var sound2 = document.getElementById("shock");
@@ -11,12 +11,12 @@ var b2 = document.getElementById("shockdart");
 var b3 = document.getElementById("drone");
 var b4 = document.getElementById("fury");
 
-p1.addEventListener("mousemove", function (dets) {
-  image.style.top = 30 + 1 - dets.y * 0.01 + "px";
-  image.style.left = 350 + 1 - dets.x * 0.01 + "px";
-});
+// p1.addEventListener("mousemove", function (dets) {
+//   image.style.top = 30 + 1 - dets.y * 0.01 + "px";
+//   image.style.left = 350 + 1 - dets.x * 0.01 + "px";
+// });
 
-b1.addEventListener("dblclick", (e) => {
+b1.addEventListener("touch", (e) => {
   e.preventDefault();
   sound1.play();
 });
